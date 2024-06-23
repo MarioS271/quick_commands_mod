8115a2b78208e23f6083c663422c48db4a52337a
MarioS271 <marioerik.scholze@gmail.com>
2024-06-23 22:25:44 +0200

    Finished Time and Weather

--- src/main/java/net/marios271/quick_commands/screen/QuickCommandsScreen.java
+++ src/main/java/net/marios271/quick_commands/screen/QuickCommandsScreen.java
@@ -20,37 +20,69 @@ public class QuickCommandsScreen extends Screen {
     protected void init() {
         TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
 
-        TextWidget text_time = new TextWidget(Text.literal("test"), textRenderer);
-        ButtonWidget button_day = ButtonWidget.builder(Text.translatable("button.quick_commands.time_day"), button -> {
+        // TIME OPTIONS
+        TextWidget text_time = new TextWidget(Text.translatable("text.quick_commands.set_time"), textRenderer);
+        text_time.setX(width / 2 - 120);
+        text_time.setY(10);
+        ButtonWidget button_time_day = ButtonWidget.builder(Text.translatable("button.quick_commands.time_day"), button -> {
                     execute("time set day");
                 })
-                .dimensions(width / 2 - 200, 20, 150, 20)
+                .dimensions(width / 2 - 200, 25, 180, 20)
                 .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.time_day")))
                 .build();
-        ButtonWidget button_noon = ButtonWidget.builder(Text.translatable("button.quick_commands.time_noon"), button -> {
+        ButtonWidget button_time_noon = ButtonWidget.builder(Text.translatable("button.quick_commands.time_noon"), button -> {
                     execute("time set noon");
                 })
-                .dimensions(width / 2 - 200, 45, 150, 20)
+                .dimensions(width / 2 - 200, 50, 180, 20)
                 .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.time_noon")))
                 .build();
-        ButtonWidget button_night = ButtonWidget.builder(Text.translatable("button.quick_commands.time_night"), button -> {
+        ButtonWidget button_time_night = ButtonWidget.builder(Text.translatable("button.quick_commands.time_night"), button -> {
                     execute("time set night");
                 })
-                .dimensions(width / 2 - 200, 70, 150, 20)
+                .dimensions(width / 2 - 200, 75, 180, 20)
                 .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.time_night")))
                 .build();
-        ButtonWidget button_midnight = ButtonWidget.builder(Text.translatable("button.quick_commands.time_midnight"), button -> {
+        ButtonWidget button_time_midnight = ButtonWidget.builder(Text.translatable("button.quick_commands.time_midnight"), button -> {
                     execute("time set midnight");
                 })
-                .dimensions(width / 2 - 200, 95, 150, 20)
+                .dimensions(width / 2 - 200, 100, 180, 20)
                 .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.time_midnight")))
                 .build();
 
         addDrawableChild(text_time);
-        addDrawableChild(button_day);
-        addDrawableChild(button_noon);
-        addDrawableChild(button_night);
-        addDrawableChild(button_midnight);
+        addDrawableChild(button_time_day);
+        addDrawableChild(button_time_noon);
+        addDrawableChild(button_time_night);
+        addDrawableChild(button_time_midnight);
+
+
+        // WEATHER OPTIONS
+        TextWidget text_weather = new TextWidget(Text.translatable("text.quick_commands.set_weather"), textRenderer);
+        text_weather.setX(width / 2 - 128);
+        text_weather.setY(140);
+        ButtonWidget button_weather_clear = ButtonWidget.builder(Text.translatable("button.quick_commands.weather_clear"), button -> {
+                    execute("weather clear");
+                })
+                .dimensions(width / 2 - 200, 155, 180, 20)
+                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.weather_clear")))
+                .build();
+        ButtonWidget button_weather_rain = ButtonWidget.builder(Text.translatable("button.quick_commands.weather_rain"), button -> {
+                    execute("weather rain");
+                })
+                .dimensions(width / 2 - 200, 180, 180, 20)
+                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.weather_rain")))
+                .build();
+        ButtonWidget button_weather_thunder = ButtonWidget.builder(Text.translatable("button.quick_commands.weather_thunder"), button -> {
+                    execute("weather thunder");
+                })
+                .dimensions(width / 2 - 200, 205, 180, 20)
+                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.weather_thunder")))
+                .build();
+
+        addDrawableChild(text_weather);
+        addDrawableChild(button_weather_clear);
+        addDrawableChild(button_weather_rain);
+        addDrawableChild(button_weather_thunder);
     }
 
     private void execute(String command){
