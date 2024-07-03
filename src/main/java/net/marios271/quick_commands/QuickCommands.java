a867be54973571c411a7584daabe70e679ceae36
MarioS271 <marioerik.scholze@gmail.com>
2024-07-03 21:04:30 +0200

    [INDEV] v1.2: Removed Night Vision Feature and moved it to a seperate mod (CatVision)

--- src/main/java/net/marios271/quick_commands/QuickCommands.java
+++ src/main/java/net/marios271/quick_commands/QuickCommands.java
@@ -1,13 +1,7 @@
 package net.marios271.quick_commands;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.marios271.quick_commands.config.ModConfigs;
 import net.marios271.quick_commands.event.KeyInputHandler;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,19 +11,8 @@ public class QuickCommands implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        ModConfigs.registerConfigs();
         KeyInputHandler.register();
 
         LOGGER.info("Initialized " + MOD_ID);
-
-        ClientPlayConnectionEvents.JOIN.register((arg1, arg2, arg3) -> {
-            ClientPlayerEntity player = MinecraftClient.getInstance().player;
-            assert player != null;
-
-            if (!ModConfigs.REMEMBER_NV){ ModConfigs.NV_STATUS = false; }
-            if (ModConfigs.AUTO_NV){ ModConfigs.NV_STATUS = true; }
-
-            if (ModConfigs.NV_STATUS){ player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, -1)); }
-        });
     }
 }
--- src/main/java/net/marios271/quick_commands/event/KeyInputHandler.java
+++ src/main/java/net/marios271/quick_commands/event/KeyInputHandler.java
@@ -2,38 +2,21 @@ package net.marios271.quick_commands.event;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.marios271.quick_commands.config.ModConfigs;
 import net.marios271.quick_commands.screen.QuickCommandsScreen;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
     public static final String KEY_CATEGORY_QUICK_COMMANDS = "key.category.quick_commands";
     public static final String KEY_OPEN_GUI = "key.quick_commands.open_gui";
-    public static final String KEY_TOGGLE_CLIENT_NV = "key.quick_commands.toggle_client_night_vision";
 
     public static KeyBinding openGuiKey;
-    public static KeyBinding toggleNVClientKey;
 
     public static void registerKeyInputs(){
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (openGuiKey.wasPressed()) {
                 client.setScreen(new QuickCommandsScreen());
-            }
-
-            if (toggleNVClientKey.wasPressed()) {
-                assert client.player != null;
-
-                if (!ModConfigs.NV_STATUS) {
-                    ModConfigs.NV_STATUS = true;
-                    client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, -1));
-                } else {
-                    ModConfigs.NV_STATUS = false;
-                    client.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
-                }
             }
         });
     }
@@ -43,12 +26,6 @@ public class KeyInputHandler {
                 KEY_OPEN_GUI,
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_F8,
-                KEY_CATEGORY_QUICK_COMMANDS
-        ));
-        toggleNVClientKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                KEY_TOGGLE_CLIENT_NV,
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_COMMA,
                 KEY_CATEGORY_QUICK_COMMANDS
         ));
 
