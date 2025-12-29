0e5638928e2735a178fb9ca922431ce0b9c9e29a
MarioS271 <marioerik.scholze@gmail.com>
2025-12-29 20:00:39 +0100

    Update to 1.21.10 and 1.21.11 (farewell old version nums...)
    Removed accesswidener cause no idea why I even needed it??
    Updated loom to 1.14-SNAPSHOT and gradle to 9.2
    Migrated to official mojang mappings

--- src/main/java/net/marios271/quick_commands/event/KeyInputHandler.java
+++ src/main/java/net/marios271/quick_commands/event/KeyInputHandler.java
@@ -1,31 +1,31 @@
 package net.marios271.quick_commands.event;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.marios271.quick_commands.screen.QuickCommandsScreen;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
-    public static final KeyBinding.Category QUICK_COMMANDS_CATEGORY = KeyBinding.Category.create(Identifier.of("key.category.quick_commands"));
+    public static final KeyMapping.Category QUICK_COMMANDS_CATEGORY = KeyMapping.Category.register(Identifier.parse("key.category.quick_commands"));
     public static final String KEY_OPEN_GUI = "key.quick_commands.open_gui";
 
-    public static KeyBinding openGuiKey;
+    public static KeyMapping openGuiKey;
 
     public static void registerKeyInputs(){
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (openGuiKey.wasPressed()) {
+            if (openGuiKey.consumeClick()) {
                 client.setScreen(new QuickCommandsScreen());
             }
         });
     }
 
     public static void register(){
-        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 KEY_OPEN_GUI,
-                InputUtil.Type.KEYSYM,
+                InputConstants.Type.KEYSYM,
                 GLFW.GLFW_KEY_F8,
                 QUICK_COMMANDS_CATEGORY
         ));
--- src/main/java/net/marios271/quick_commands/screen/QuickCommandsScreen.java
+++ src/main/java/net/marios271/quick_commands/screen/QuickCommandsScreen.java
@@ -1,80 +1,81 @@
 package net.marios271.quick_commands.screen;
 
 import net.marios271.quick_commands.event.KeyInputHandler;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.tooltip.Tooltip;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.client.gui.widget.TextWidget;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.components.StringWidget;
+import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.input.KeyEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 
 public class QuickCommandsScreen extends Screen {
     public QuickCommandsScreen() {
-        super(Text.translatable("gui_title.quick_commands.commands"));
+        super(Component.translatable("gui_title.quick_commands.commands"));
     }
 
     @Override
     protected void init() {
-        MinecraftClient client = MinecraftClient.getInstance();
+        Minecraft client = Minecraft.getInstance();
 
         // --- TIME OPTIONS ---
-        Text textTimeText = Text.translatable("text.quick_commands.set_time");
-        TextWidget text_time = new TextWidget(textTimeText, textRenderer);
-        text_time.setPosition(width / 2 - textRenderer.getWidth(textTimeText) / 2 - 110, height / 2 - 110);
+        Component ComponentTimeComponent = Component.translatable("text.quick_commands.set_time");
+        StringWidget Component_time = new StringWidget(ComponentTimeComponent, font);
+        Component_time.setPosition(width / 2 - font.width(ComponentTimeComponent) / 2 - 110, height / 2 - 110);
 
-        ButtonWidget button_time_day = ButtonWidget.builder(Text.translatable("button.quick_commands.time_day"), button -> execute("time set day"))
-                .dimensions(width / 2 - 200, height / 2 - 95, 180, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.time_day")))
+        Button button_time_day = Button.builder(Component.translatable("button.quick_commands.time_day"), button -> execute("time set day"))
+                .bounds(width / 2 - 200, height / 2 - 95, 180, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.time_day")))
                 .build();
-        ButtonWidget button_time_noon = ButtonWidget.builder(Text.translatable("button.quick_commands.time_noon"), button -> execute("time set noon"))
-                .dimensions(width / 2 - 200, height / 2 - 70, 180, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.time_noon")))
+        Button button_time_noon = Button.builder(Component.translatable("button.quick_commands.time_noon"), button -> execute("time set noon"))
+                .bounds(width / 2 - 200, height / 2 - 70, 180, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.time_noon")))
                 .build();
-        ButtonWidget button_time_night = ButtonWidget.builder(Text.translatable("button.quick_commands.time_night"), button -> execute("time set night"))
-                .dimensions(width / 2 - 200, height / 2 - 45, 180, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.time_night")))
+        Button button_time_night = Button.builder(Component.translatable("button.quick_commands.time_night"), button -> execute("time set night"))
+                .bounds(width / 2 - 200, height / 2 - 45, 180, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.time_night")))
                 .build();
-        ButtonWidget button_time_midnight = ButtonWidget.builder(Text.translatable("button.quick_commands.time_midnight"), button -> execute("time set midnight"))
-                .dimensions(width / 2 - 200, height / 2 - 20, 180, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.time_midnight")))
+        Button button_time_midnight = Button.builder(Component.translatable("button.quick_commands.time_midnight"), button -> execute("time set midnight"))
+                .bounds(width / 2 - 200, height / 2 - 20, 180, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.time_midnight")))
                 .build();
 
-        addDrawableChild(text_time);
-        addDrawableChild(button_time_day);
-        addDrawableChild(button_time_noon);
-        addDrawableChild(button_time_night);
-        addDrawableChild(button_time_midnight);
+        addRenderableWidget(Component_time);
+        addRenderableWidget(button_time_day);
+        addRenderableWidget(button_time_noon);
+        addRenderableWidget(button_time_night);
+        addRenderableWidget(button_time_midnight);
 
         // --- WEATHER OPTIONS ---
-        Text textWeatherText = Text.translatable("text.quick_commands.set_weather");
-        TextWidget text_weather = new TextWidget(textWeatherText, textRenderer);
-        text_weather.setPosition(width / 2 - textRenderer.getWidth(textWeatherText) / 2 - 110, height / 2 + 20);
+        Component ComponentWeatherComponent = Component.translatable("text.quick_commands.set_weather");
+        StringWidget Component_weather = new StringWidget(ComponentWeatherComponent, font);
+        Component_weather.setPosition(width / 2 - font.width(ComponentWeatherComponent) / 2 - 110, height / 2 + 20);
 
-        ButtonWidget button_weather_clear = ButtonWidget.builder(Text.translatable("button.quick_commands.weather_clear"), button -> execute("weather clear"))
-                .dimensions(width / 2 - 200, height / 2 + 35, 180, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.weather_clear")))
+        Button button_weather_clear = Button.builder(Component.translatable("button.quick_commands.weather_clear"), button -> execute("weather clear"))
+                .bounds(width / 2 - 200, height / 2 + 35, 180, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.weather_clear")))
                 .build();
-        ButtonWidget button_weather_rain = ButtonWidget.builder(Text.translatable("button.quick_commands.weather_rain"), button -> execute("weather rain"))
-                .dimensions(width / 2 - 200, height / 2 + 60, 180, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.weather_rain")))
+        Button button_weather_rain = Button.builder(Component.translatable("button.quick_commands.weather_rain"), button -> execute("weather rain"))
+                .bounds(width / 2 - 200, height / 2 + 60, 180, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.weather_rain")))
                 .build();
-        ButtonWidget button_weather_thunder = ButtonWidget.builder(Text.translatable("button.quick_commands.weather_thunder"), button -> execute("weather thunder"))
-                .dimensions(width / 2 - 200, height / 2 + 85, 180, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.weather_thunder")))
+        Button button_weather_thunder = Button.builder(Component.translatable("button.quick_commands.weather_thunder"), button -> execute("weather thunder"))
+                .bounds(width / 2 - 200, height / 2 + 85, 180, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.weather_thunder")))
                 .build();
 
-        addDrawableChild(text_weather);
-        addDrawableChild(button_weather_clear);
-        addDrawableChild(button_weather_rain);
-        addDrawableChild(button_weather_thunder);
+        addRenderableWidget(Component_weather);
+        addRenderableWidget(button_weather_clear);
+        addRenderableWidget(button_weather_rain);
+        addRenderableWidget(button_weather_thunder);
 
         // --- EFFECT OPTIONS ---
-        Text textEffectsText = Text.translatable("text.quick_commands.set_effects");
-        TextWidget text_effects = new TextWidget(textEffectsText, textRenderer);
-        text_effects.setPosition(width / 2 - textRenderer.getWidth(textEffectsText) / 2 + 94, height / 2 - 110);
-        addDrawableChild(text_effects);
+        Component ComponentEffectsComponent = Component.translatable("text.quick_commands.set_effects");
+        StringWidget Component_effects = new StringWidget(ComponentEffectsComponent, font);
+        Component_effects.setPosition(width / 2 - font.width(ComponentEffectsComponent) / 2 + 94, height / 2 - 110);
+        addRenderableWidget(Component_effects);
 
         addEffectWidget("resistance", 95);
         addEffectWidget("regeneration", 70);
@@ -82,88 +83,93 @@ public class QuickCommandsScreen extends Screen {
         addEffectWidget("jump_boost", 20);
 
         // --- SCALE SETTER ---
-        Text textItemsText = Text.translatable("text.quick_commands.set_scale");
-        TextWidget text_items = new TextWidget(textItemsText, textRenderer);
-        text_items.setPosition(width / 2 - textRenderer.getWidth(textItemsText) / 2 + 94, height / 2 + 20);
-        addDrawableChild(text_items);
+        Component ComponentItemsComponent = Component.translatable("text.quick_commands.set_scale");
+        StringWidget Component_items = new StringWidget(ComponentItemsComponent, font);
+        Component_items.setPosition(width / 2 - font.width(ComponentItemsComponent) / 2 + 94, height / 2 + 20);
+        addRenderableWidget(Component_items);
 
-        TextFieldWidget textfield_scale = new TextFieldWidget(textRenderer, width / 2 + 12, height / 2 + 35, 180, 20, Text.literal(""));
-        textfield_scale.setTooltip(Tooltip.of(Text.translatable("textfield.tooltip.quick_commands.scale")));
-        textfield_scale.setTextPredicate((text) -> text.matches("([0-9]*\\.?[0-9]*)?"));
+        EditBox Textfield_scale = new EditBox(font, width / 2 + 12, height / 2 + 35, 180, 20, Component.literal(""));
+        Textfield_scale.setTooltip(Tooltip.create(Component.translatable("textfield.tooltip.quick_commands.scale")));
+        Textfield_scale.setFilter((Component) -> Component.matches("([0-9]*\\.?[0-9]*)?"));
 
-        ButtonWidget button_set_scale = ButtonWidget.builder(Text.translatable("button.quick_commands.set_scale"), button -> {
-                    String input = textfield_scale.getText();
+        Button button_set_scale = Button.builder(Component.translatable("button.quick_commands.set_scale"), button -> {
+                    String input = Textfield_scale.getValue();
                     if (!input.isEmpty()) {
                         try {
                             float scale = Float.parseFloat(input);
                             execute("attribute @s minecraft:scale base set " + scale);
                         } catch (NumberFormatException e) {
-                            sendErrorMsg(Text.translatable("text.quick_commands.status.invalid_scale_number"));
+                            sendErrorMsg(Component.translatable("text.quick_commands.status.invalid_scale_number"));
                         }
                     } else {
-                        sendErrorMsg(Text.translatable("text.quick_commands.status.no_scale_value"));
+                        sendErrorMsg(Component.translatable("text.quick_commands.status.no_scale_value"));
                     }
-                }).dimensions(width / 2 + 12, height / 2 + 60, 88, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.set_scale")))
+                }).bounds(width / 2 + 12, height / 2 + 60, 88, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.set_scale")))
                 .build();
 
-        ButtonWidget button_reset_scale = ButtonWidget.builder(Text.translatable("button.quick_commands.reset_scale"), button -> execute("attribute @s minecraft:scale base set 1.0"))
-                .dimensions(width / 2 + 104, height / 2 + 60, 88, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.reset_scale")))
+        Button button_reset_scale = Button.builder(Component.translatable("button.quick_commands.reset_scale"), button -> execute("attribute @s minecraft:scale base set 1.0"))
+                .bounds(width / 2 + 104, height / 2 + 60, 88, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.reset_scale")))
                 .build();
 
-        addDrawableChild(textfield_scale);
-        addDrawableChild(button_set_scale);
-        addDrawableChild(button_reset_scale);
+        addRenderableWidget(Textfield_scale);
+        addRenderableWidget(button_set_scale);
+        addRenderableWidget(button_reset_scale);
     }
 
     private void addEffectWidget(String effectName, int yOffset) {
-        TextWidget text_effect = new TextWidget(Text.translatable("text.quick_commands.effects." + effectName), textRenderer);
-        text_effect.setPosition(width / 2 + 34, height / 2 - yOffset + 7);
+        StringWidget Component_effect = new StringWidget(Component.translatable("text.quick_commands.effects." + effectName), font);
+        Component_effect.setPosition(width / 2 + 34, height / 2 - yOffset + 7);
 
-        TextFieldWidget textField = new TextFieldWidget(textRenderer, width / 2, height / 2 - yOffset, 30, 20, Text.literal(""));
-        textField.setTextPredicate((text) -> text.matches("[0-9]*"));
-        textField.setMaxLength(3);
-        textField.setTooltip(Tooltip.of(Text.translatable("textfield.tooltip.quick_commands.effect_strength")));
+        EditBox ComponentField = new EditBox(font, width / 2, height / 2 - yOffset, 30, 20, Component.literal(""));
+        ComponentField.setFilter((Component) -> Component.matches("[0-9]*"));
+        ComponentField.setMaxLength(3);
+        ComponentField.setTooltip(Tooltip.create(Component.translatable("textfield.tooltip.quick_commands.effect_strength")));
 
-        ButtonWidget buttonGive = ButtonWidget.builder(Text.translatable("button.quick_commands.give"), button -> {
+        Button buttonGive = Button.builder(Component.translatable("button.quick_commands.give"), button -> {
                     try {
-                        execute("effect give @s " + effectName + " infinite " + Integer.parseInt(textField.getText()) + " true");
+                        execute("effect give @s " + effectName + " infinite " + Integer.parseInt(ComponentField.getValue()) + " true");
                     } catch (NumberFormatException e) {
-                        sendErrorMsg(Text.translatable("text.quick_commands.status.no_effect_value"));
+                        sendErrorMsg(Component.translatable("text.quick_commands.status.no_effect_value"));
                     }
-                }).dimensions(width / 2 + 108, height / 2 - yOffset, 50, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.effect_give")))
+                }).bounds(width / 2 + 108, height / 2 - yOffset, 50, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.effect_give")))
                 .build();
 
-        ButtonWidget buttonClear = ButtonWidget.builder(Text.translatable("button.quick_commands.clear"), button -> execute("effect clear @s " + effectName))
-                .dimensions(width / 2 + 160, height / 2 - yOffset, 50, 20)
-                .tooltip(Tooltip.of(Text.translatable("button.tooltip.quick_commands.effect_clear")))
+        Button buttonClear = Button.builder(Component.translatable("button.quick_commands.clear"), button -> execute("effect clear @s " + effectName))
+                .bounds(width / 2 + 160, height / 2 - yOffset, 50, 20)
+                .tooltip(Tooltip.create(Component.translatable("button.tooltip.quick_commands.effect_clear")))
                 .build();
 
-        addDrawableChild(text_effect);
-        addDrawableChild(textField);
-        addDrawableChild(buttonGive);
-        addDrawableChild(buttonClear);
+        addRenderableWidget(Component_effect);
+        addRenderableWidget(ComponentField);
+        addRenderableWidget(buttonGive);
+        addRenderableWidget(buttonClear);
     }
 
     @Override
-    public boolean keyPressed(net.minecraft.client.input.KeyInput input) {
-        if (KeyInputHandler.openGuiKey.matchesKey(input)) {
-            MinecraftClient.getInstance().setScreen(null);
+    public boolean keyPressed(KeyEvent keyEvent) {
+        if (KeyInputHandler.openGuiKey.matches(keyEvent)) {
+            this.onClose();
             return true;
         }
-        return super.keyPressed(input);
+        return super.keyPressed(keyEvent);
     }
-
 
     private void execute(String command) {
-        MinecraftClient.getInstance().player.networkHandler.sendChatCommand(command);
-        MinecraftClient.getInstance().setScreen(null);
+        Minecraft client = Minecraft.getInstance();
+        assert client.player != null;
+
+        client.player.connection.sendCommand(command);
+        client.setScreen(null);
     }
 
-    private void sendErrorMsg(MutableText text) {
-        MinecraftClient.getInstance().player.sendMessage(text, false);
-        MinecraftClient.getInstance().setScreen(null);
+    private void sendErrorMsg(MutableComponent text) {
+        Minecraft client = Minecraft.getInstance();
+        assert client.player != null;
+
+        client.player.displayClientMessage(text, false);
+        client.setScreen(null);
     }
 }
